ff0a514f9f396f5278f70e20058d128b75e57267
iliankostov <ilian.kostov@gmail.com>
2016-07-17 17:46:01 +0300

    Add compiled version

---
+++ PianoMelody.Compiled/Multimedia/readme.txt
@@ -0,0 +1,3 @@
+﻿Multimedia directory contains uploaded by administrator multimedias.
+The name of multimedia is unique identifier (Guid) which is related with database object.
+It is not recommended changing the name of any multimedia !!!
---
+++ PianoMelody.Compiled/Scripts/Custom/image-preview.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+
+    function readURL(input) {
+
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#image-preview').attr('src', e.target.result);
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $("#file").change(function () {
+        readURL(this);
+    });
+
+});
