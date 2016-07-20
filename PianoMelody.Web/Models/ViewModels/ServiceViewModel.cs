d4c462ff4043df3a5400287e52d01d2a05718d00
iliankostov <ilian.kostov@gmail.com>
2016-07-20 21:20:29 +0300

    Fix ServiceViewModel

--- PianoMelody.Web/Models/ViewModels/ServiceViewModel.cs
+++ PianoMelody.Web/Models/ViewModels/ServiceViewModel.cs
@@ -14,6 +14,6 @@ namespace PianoMelody.Web.Models.ViewModels
         [Localized]
         public string Description { get; set; }
 
-        public decimal Price { get; set; }
+        public decimal? Price { get; set; }
     }
 }
