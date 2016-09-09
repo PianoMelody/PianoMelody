b8ba9990099b6584a1f2f05c75226da7f329bb36
iliankostov <ilian.kostov@gmail.com>
2016-09-09 11:44:50 +0300

    Rollback default menu

--- PianoMelody.Web/App_Start/BundleConfig.cs
+++ PianoMelody.Web/App_Start/BundleConfig.cs
@@ -61,7 +61,6 @@
 
             bundles.Add(
                 new StyleBundle("~/Content/css").Include(
-                    "~/Content/menu.css",
                     "~/Content/bootstrap.min.css",
                     "~/Content/photoswipe/photoswipe.css",
                     "~/Content/photoswipe/default-skin/default-skin.css",
