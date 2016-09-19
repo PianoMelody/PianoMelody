3eba094acdb3e1aee7d1a02f2fd4107e6e65ed43
Emilia Pavlova <emilia.pavlova@gmail.com>
2016-09-19 21:18:31 +0300

    fix dropdown menu active class

--- PianoMelody.Compiled/Scripts/Custom/shared.js
+++ PianoMelody.Compiled/Scripts/Custom/shared.js
@@ -1,8 +1,16 @@
 ﻿//// Set active selected href
 $(document).ready(function() {
     $("[href]").each(function() {
-        if (this.href == window.location.href) {
+        if (this.href === window.location.href) {
             $(this).addClass("active");
+        }
+    });
+});
+
+$(document).ready(function(){
+    $(".dropdown-menu li [href]").each(function() {
+        if (this.href === window.location.href) {
+            $(this).parent().parent().prev().addClass("active");
         }
     });
 });
